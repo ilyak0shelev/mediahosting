7809e067c0976384b45623c7344edadb6d0802b3
ilyak0shelev <koshelev081002@gmail.com>
2023-11-14 23:18:40 +0700

    Connected Node.js server to MongoDB

--- server/authRouter.js
+++ server/authRouter.js
@@ -1,9 +1,15 @@
 const Router = require('express');
 const router = new Router()
+const User = require('./models/user')
 
 router.post('/login', (req, res) => {
-    console.log(req.body.nickname)
-    console.log(req.body.pswd)
+    // console.log(req.body.nickname)
+    // console.log(req.body.pswd)
+    const {nickname, pswd} = req.body
+    const user = new User({nickname, pswd});
+    user.collection.findOne({nickname, pswd})
+    .then((result) => console.log(result))
+    .catch((error) => console.log(error))
 });
 
 router.post('/registration', (req, res) => {
--- server/server.js
+++ server/server.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const app = express(); 
 const authRouter = require('./authRouter')
-const port = process.env.PORT || 5000; 
+const port = process.env.PORT || 5000
+const mongoose = require('mongoose')
+const User = require('./models/user')
 
 app.use(express.json())
 app.use("/auth", authRouter)
 
-const start = async () => {
-    try {
+const start = () => {
+        mongoose.connect(`mongodb+srv://admin:cYHpzm6K3Ooi@cluster0.q13jkdo.mongodb.net/user_data?retryWrites=true&w=majority`)
+        .then((res) => console.log("MongoDB connected!"))
+        .catch((error) => console.log(error))
+        
         app.listen(port, () => console.log(`Listening on port ${port}`));
-    } catch (error) {
-        console.log(error)
     }
-}
 
 start()
 
