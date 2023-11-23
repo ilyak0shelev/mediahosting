53d46aef301c27c373ec61f85f6201bae48f0797
ilyak0shelev <koshelev081002@gmail.com>
2023-11-24 00:54:23 +0700

    Some edits

--- src/components/UI/modal/Modal.jsx
+++ src/components/UI/modal/Modal.jsx
@@ -1,13 +1,8 @@
-import React, { useContext } from 'react'
 import "./Modal.css"
-import { PageContext } from '../../contexts/PageContext'
 
 const Modal = ({ children, visibility, changeVisibility }) => {
 
-  const value = useContext(PageContext)
-
   const handlerClicker = () => {
-    value.changeActive(null)
     changeVisibility('inactive')
   }
   
