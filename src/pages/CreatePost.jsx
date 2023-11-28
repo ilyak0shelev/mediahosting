9be4fb5622edf9ae38e9452957f75b74e094ded4
ilyak0shelev <koshelev081002@gmail.com>
2023-11-28 22:14:15 +0700

    File preview & descrption, uploading a file to the server is working

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -1,36 +1,74 @@
-import React, {useContext, useRef, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import '../styles/CreatePost.css'
 import axios from 'axios'
-import { AuthStatusContext } from '../components/contexts/AuthStatusContext'
 import Button from '../components/UI/button/Button'
 
 const CreatePost = () => {
-  const status = useContext(AuthStatusContext)
+  const controller = new AbortController();
   const [file, setFile] = useState('')
-  const [data, getFile] = useState({ name: "", path: "" });
+  const [data, getFile] = useState({ type: "", name: "", path: "" })
+  const [selected, setSelected] = useState(false)
+  const [progress, setProgess] = useState(0)
+  const [published, setPublished] = useState('')
+  const [publishedMsg, setPublishedMsg] = useState('')
+  const [nameValue, setNameValue] = useState('')
+  const [descValue, setDescValue] = useState('')
+  const [tagsValue, setTagsValue] = useState('')
 
-  const [progress, setProgess] = useState(0); // progessbar
-  const el = useRef();
+  useEffect(() => {
+    setTimeout(() => {
+      setPublished(false)
+    }, 3000)
+  }, [published])
 
-  const handleChange = (e) => {
+  const handleFileChange = (e) => {
     setProgess(0)
-    const file = e.target.files[0]; // доступ к файлу
-    console.log(file);
+    const file = e.target.files[0];
     setFile(file);
+    setSelected(true)
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      getFile({ type: file.type, path: reader.result })
+    }
+    reader.readAsDataURL(file);
+  }
+
+  const clearForm = () => {
+    setNameValue('')
+    setDescValue('')
+    setTagsValue('')
+    setSelected(false)
+    getFile({ type: "", name: "", path: "" })
+    setFile('')
+    setProgess(0)
   }
 
   const uploadFile = (e) => {
     e.preventDefault()
     const formData = new FormData()
     formData.append('file', file);
-    axios.post('', formData, {
+    formData.append('name', nameValue)
+    formData.append('description', descValue)
+    formData.append('tags', tagsValue)
+    axios.post('/post/upload', formData, {
       onUploadProgress: (ProgressEvent) => {
         let progress = Math.round(
           ProgressEvent.loaded / ProgressEvent.total * 100
         ) + '%';
         setProgess(progress);
-      }
+      },
+      signal: controller.signal
     })
+      .then((res) => {
+        setPublished(true)
+        setPublishedMsg('Пост опубликован!')
+        clearForm()
+      })
+      .catch(() => {
+        setPublished(true)
+        setPublishedMsg('Ой... Что-то пошло не так...')
+        clearForm()
+      })
   }
 
   return (
@@ -39,24 +77,32 @@ const CreatePost = () => {
         <h1 className='newPostTitle'>Создание поста</h1>
       </div>
       <div className='newPostMainCont'>
-        <div className='uploadField'>
-          <label className='fileLabel'>
-            <div className='fileExplanation'>
-              <img src='/imgs/upload-2.png' alt='upload'/>
-              <h1>Нажмите, чтобы выбрать файл, или просто перетащите его сюда!</h1>
-            </div>
-            <input onChange={handleChange} className='fileInput' type='file' accept='.mp4, .avi, .jpeg, .jpg, .png'/>
-          </label>
-        </div>
+        {data.type.match('video') && <video className='uploadField' src={data.path} alt={data.name} loop controls />}
+        {data.type.match('image') && <img className='uploadField' src={data.path} alt={data.name} />}
+        {!data.path &&
+          <div className='uploadField'>
+            <label className='fileLabel'>
+              <div className='fileExplanation'>
+                <img src='/imgs/upload-2.png' alt='upload' />
+                <h1>Нажмите, чтобы выбрать файл, или просто перетащите его сюда!</h1>
+              </div>
+              <input onChange={handleFileChange} className='fileInput' type='file' accept='.mp4, .avi, .jpeg, .jpg, .png' />
+            </label>
+          </div>
+        }
         <div className='newPostDescriptionCont'>
-          <form className='descriptionForm'>
-            <textarea required type='text' placeholder='Название поста...' name='postName' className='newPostInput'/>
-            <textarea required type='text' placeholder='Описание...' name='postDescription' className='newPostInput'/>
-            <textarea required type='text' placeholder='Теги...' name='postTags' className='newPostInput'/>
-            <Button id='createBtn'>Создать пост</Button>
+          <form className='descriptionForm' onSubmit={uploadFile}>
+            <textarea value={nameValue} onChange={(e) => { setNameValue(e.target.value) }} required type='text' placeholder='Название поста...' name='postName' className='newPostInput' />
+            <textarea value={descValue} onChange={(e) => { setDescValue(e.target.value) }} type='text' placeholder='Описание...' name='postDescription' className='newPostInput' />
+            <textarea value={tagsValue} onChange={(e) => { setTagsValue(e.target.value) }} type='text' placeholder='Теги...' name='postTags' className='newPostInput' />
+            {selected && <div className='progessBarCont'><div className="progessBar" style={{ width: progress }}></div></div>}
+            <Button disabled={!selected} id='createBtn'>Опубликовать пост</Button>
           </form>
         </div>
       </div>
+      {published &&
+        <div className='publishedAlert'>{publishedMsg}</div>
+      }
     </div>
   )
 }
