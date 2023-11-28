b8c369eaa630bfdef0a26ae68f947c3b7e65d857
ilyak0shelev <koshelev081002@gmail.com>
2023-11-28 22:07:02 +0700

    Routes update

--- server/server.js
+++ server/server.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const app = express(); 
 const authRouter = require('./authRouter')
+const postRouter = require('./postRouter')
 const port = process.env.PORT || 5000
 const mongoose = require('mongoose')
+const session = require('express-session')
+const fileUpload = require('express-fileupload');
 
+app.use(
+    session({
+        cookie: {
+            sameSite: 'strict'
+        },
+        secret: 'Gz10FhNcyZ',
+        saveUninitialized: true,
+    })
+)
 
 app.use(express.json())
+app.use(fileUpload());
 app.use("/auth", authRouter)
+app.use("/post", postRouter)
 
 const start = () => {
         mongoose.connect(`mongodb+srv://admin:cYHpzm6K3Ooi@cluster0.q13jkdo.mongodb.net/user_data?retryWrites=true&w=majority`)
