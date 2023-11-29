32e18129e691478a12d6baa9930c55832eb89009
ilyak0shelev <koshelev081002@gmail.com>
2023-11-29 21:48:28 +0700

    Upd logic for writing to the database

--- server/postRouter.js
+++ server/postRouter.js
@@ -1,28 +1,26 @@
 const Router = require('express');
 const router = new Router()
-const fs = require('fs');
 const path = require('path')
 const File = require('./models/file')
+const crypto = require("crypto");
 
 router.post('/upload', (req, res) => {
-    const {name, description, tags} = req.body
+    const { name, description, tags } = req.body
     const type = req.files.file.mimetype
     const owner = req.session.login
     const myFile = req.files.file
+    const id = crypto.randomBytes(16).toString("hex");
 
-    File.collection.findOne({name})
-    .then((result) => {
-        if (!result) {
-            myFile.mv(path.resolve(__dirname + `/storage/${req.session.login}/${name}`))
-            .then(() => {
-                const file = new File({name, description, tags, type, owner})
-                file.save()
+    const file = new File({ name, description, tags, type, owner, id })
+    file.save()
+        .then(() => {
+            myFile.mv(path.resolve(__dirname + `/storage/${req.session.login}/${id}`))
                 .then(() => res.send('Saved'))
-                .catch((error) => res.status(400).send(error))
-            })
-            .catch((error) => res.status(400).send(error))
-        }
-    })
+                .catch((error) => {
+                    file.deleteOne({id}); 
+                    res.status(400).send(error)})
+        })
+        .catch((error) => res.status(400).send(error))
 })
 
 module.exports = router
