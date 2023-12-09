356c50054db102f57ed7cb1633d1bdd2a71a882c
ilyak0shelev <koshelev081002@gmail.com>
2023-12-10 02:30:18 +0700

    Edit Profile Bar UI

--- src/components/ProfileBar.jsx
+++ src/components/ProfileBar.jsx
@@ -1,10 +1,10 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import Button from './UI/button/Button'
 import { PageContext } from './contexts/PageContext'
 import { AuthWindowContext } from './contexts/AuthWindowContext'
 import { AuthStatusContext } from './contexts/AuthStatusContext'
 import { useNavigate } from 'react-router-dom'
-
+import useOutsideClick from './hooks/useOutsideClick'
 import axios from 'axios'
 
 export const ProfileBar = () => {
@@ -12,6 +12,7 @@ export const ProfileBar = () => {
   const auth_visibility = useContext(AuthWindowContext)
   const session_status = useContext(AuthStatusContext)
   const navigate = useNavigate()
+  const { ref, isActive, setIsActive } = useOutsideClick(false);
 
   const myProfileBtnHandler = () => {
     navigate(`/profiles/${session_status.authStatus.login}`)
@@ -31,6 +32,7 @@ export const ProfileBar = () => {
     axios.get('/auth/logout')
       .then(() => {
         session_status.changeAuthStatus(false)
+        setIsActive(false)
       })
       .catch()
   }
@@ -41,11 +43,21 @@ export const ProfileBar = () => {
 
   return (
     <div className='profileBarCont'>
-      {session_status.authStatus.authorized === true &&
-        <div className='profileBar'>
+      {session_status.authStatus.authorized &&
+        <div ref={ref} className='profileBar'>
           <Button id='newPostBtn' data-title='Создать пост' onClick={newPostBthHandler}></Button>
-          <Button id='myProfileBtn' data-title='Профиль' onClick={myProfileBtnHandler}>Профиль</Button>
-          <Button id='logoutBtn' onClick={logoutBthHandler}>Выйти</Button>
+          <Button id='myProfileBtn' data-title='Профиль' onClick={myProfileBtnHandler}></Button>
+          <button className='menuBtn' onClick={() => setIsActive(!isActive)}></button>
+          <nav className={`menu ${isActive ? 'active' : ''}`}>
+            <ul className='menuList'>
+              <li className='menuItem'>
+                <span id='settingsBtn'>Настройки</span>
+              </li>
+              <li className='menuItem'>
+                <span id='logoutBtn' onClick={logoutBthHandler}>Выйти</span>
+              </li>
+            </ul>
+          </nav>
         </div>
       }
       {!session_status.authStatus.authorized &&
